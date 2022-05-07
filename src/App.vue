d74d791b76d01f50d7869bcd0b1d16aaae505354
Eli <mcditto99@gmail.com>
2022-05-07 11:38:01 -0400

    Setup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-</template>
+<script setup>
+import { reactive } from 'vue';
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+const state = reactive({ count: 0 });
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+const increment = () => {
+	state.count++;
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+	<div class="app">
+		<button @click="increment">{{ state.count }}</button>
+	</div>
+</template>
