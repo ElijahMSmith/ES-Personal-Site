5f71ceb62208d15e7d67cb98e15173e63a29bd74
ElijahMSmith <mcditto99@gmail.com>
2022-05-09 19:27:22 -0400

    removing dist from main build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: process.env.NODE_ENV === "production" ? "/ES-PersonalSite/" : "/",
-    publi
+	base: process.env.NODE_ENV === "production" ? "/ES-Personal-Site/" : "/",
 });
