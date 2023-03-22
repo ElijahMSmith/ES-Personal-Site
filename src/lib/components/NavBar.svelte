7514e57724d5f953e4576d87216b5e5982307b06
ElijahMSmith <mcditto99@gmail.com>
2023-03-22 00:51:28 -0400

    Hamburger menu for mobile

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 
+	let innerWidth: number;
+
+	$: showMobileMenu = false;
+
 	function toggleMenu() {
-		console.log("test");
+		showMobileMenu = !showMobileMenu;
 	}
 
 	onMount(() => {
@@ -19,6 +23,8 @@
 	});
 </script>
 
+<svelte:window bind:innerWidth />
+
 <div id="nav-container">
 	<a
 		id="resume-link"
@@ -27,23 +33,25 @@
 		href="resume.pdf">Resume</a
 	>
 
-	<div id="section-links-container">
+	<div id="section-links-container" class={showMobileMenu ? "active" : ""}>
 		<a class="nav-link" href="#about">About</a>
 		<a class="nav-link" href="#experiences">Experiences</a>
 		<a class="nav-link" href="#projects">Projects</a>
 		<a class="nav-link" href="#contact">Contact</a>
 	</div>
 
-	<button id="drawer-toggle" on:click={toggleMenu}>
+	<button
+		id="drawer-toggle"
+		class={showMobileMenu ? "active" : ""}
+		on:click={toggleMenu}
+	>
 		<img src="menu.svg" alt="Menu Toggle" />
 	</button>
-	<!-- TODO: Add hamburger or some other replacement for hidden navigation links -->
 </div>
 
 <style lang="scss">
 	#nav-container {
 		backdrop-filter: blur(10px);
-		// background-color: $bg-alternate;
 		position: fixed;
 		width: 100vw;
 		height: 75px;
@@ -58,21 +66,7 @@
 	}
 
 	#drawer-toggle {
-		width: 45px;
-		height: 45px;
-		border-radius: 100%;
-		background: none;
-		border: none;
-		cursor: pointer;
-
-		img {
-			height: 100%;
-			width: 100%;
-		}
-	}
-
-	#section-links-container {
-		display: none; //TODO remove
+		display: none;
 	}
 
 	#section-links-container > a {
@@ -114,15 +108,90 @@
 		padding: 0px 10px;
 		transition: background 0.2s linear, color 0.2s linear;
 		&:hover {
-			background: $text-primary;
-			color: $bg-primary;
+			background: $bg-alternate;
 		}
 	}
 
 	// Phone or very small
 	@media only screen and (max-width: 800px) {
 		#section-links-container {
-			display: none;
+			position: absolute;
+			left: 0px;
+			top: -400px;
+			width: 100%;
+			background: $bg-alternate;
+			padding-top: 10%;
+			transition: top 1s cubic-bezier(0.49, 0, 0.83, 0.31);
+
+			&.active {
+				top: 0px;
+				transition: top 1s cubic-bezier(0.16, 0.61, 0.3, 0.91);
+			}
+
+			a {
+				display: block;
+				margin-top: 10px;
+				margin-bottom: 10px;
+
+				&::before {
+					position: absolute;
+					content: "";
+					height: 2px;
+					bottom: -10px;
+					left: 0%;
+					width: 85vw;
+					background: $text-primary;
+					transition: width 0.3s ease-out;
+				}
+
+				&:last-child {
+					&::before {
+						display: none;
+					}
+				}
+
+				&::after {
+					display: none;
+				}
+			}
+		}
+
+		#drawer-toggle {
+			display: block;
+			width: 45px;
+			height: 45px;
+			border-radius: 100%;
+			background-color: $bg-primary;
+			border: 2px solid $text-primary;
+			cursor: pointer;
+			position: relative;
+			overflow: hidden;
+
+			&::after {
+				content: "";
+				position: absolute;
+				width: 100%;
+				height: 100%;
+				background-color: $bg-alternate;
+				top: -100%;
+				left: 0px;
+				border-radius: 100%;
+				transition: top 0.7s cubic-bezier(0.12, 0.92, 0.38, 0.9);
+			}
+
+			&:hover,
+			&.active {
+				&::after {
+					top: 0px;
+				}
+			}
+
+			img {
+				height: 100%;
+				width: 100%;
+				z-index: 30;
+				position: relative;
+			}
 		}
 
 		#nav-container {
